ec023f962fc3f70457bd2379282bdd09cf3e67c4
Andrew Stanton <andrew@toddprod.com>
2018-02-02 12:20:23 -0500

    Updated template for pages

--- full-video-headerpage.php
+++ full-video-headerpage.php
@@ -10,7 +10,7 @@ get_header(); ?>
 	<?php
 		get_template_part( 'template-parts/banner', 'videofull');
 	?>
-
+	<!--TEST HERE-->
 
 	<!-- <div id="primary" class="content-area"> -->
 		<main id="main" class="site-main" role="main">
